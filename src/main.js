56ca4d73512b03db3c971c7809de2ba750356a2a
Devon Wigle <devonwigle@gmail.com>
2021-10-22 15:45:44 -0500

    Make random button clickable

--- src/main.js
+++ src/main.js
@@ -6,7 +6,9 @@ var tagLine = document.querySelector ('.tagline')
 var descriptor1 = document.querySelector('.tagline-1')
 var descriptor2 = document.querySelector('.tagline-2')
 var randomButton = document.querySelector('.random-cover-button')
-
+var homeView = document.querySelector('.home-view')
+var formView = document.querySelector('.form-view')
+console.log(formView)
 // We've provided a few variables below
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
@@ -26,6 +28,7 @@ randomButton.addEventListener('click', function() {
   randomizeTitles()
   randomizeTagLine1()
   randomizeTagLine2()
+  toggleHome()
 })
 // Create your event handlers and other functions here 👇
 function randomizeCovers() {
@@ -40,7 +43,16 @@ function randomizeTagLine1() {
 function randomizeTagLine2() {
   descriptor2.innerText = descriptors[getRandomIndex(descriptors)]
 }
-
+function toggleHome() {
+//   if (homeView.contains('view') {
+//     homeView.remove('view');
+//   } else {
+//     homeView.add('view')
+//   })
+// }
+// function toggleFormView() {
+//   if ()
+// }
 // We've provided one function to get you started
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
