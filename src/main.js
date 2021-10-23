934e10f159858db31fb028ab00fb6c87caca61be
Devon Wigle <devonwigle@gmail.com>
2021-10-22 21:35:53 -0500

    Add showSavedCovers function and showHome function

--- src/main.js
+++ src/main.js
@@ -1,17 +1,17 @@
-//var Cover = require('./cover')
 // Create variables targetting the relevant DOM elements here 👇
 var coverImgSrc = document.querySelector('.cover-image')
 var title = document.querySelector('.cover-title')
-var tagLine = document.querySelector ('.tagline')
+var tagline = document.querySelector ('.tagline')
 var descriptor1 = document.querySelector('.tagline-1')
 var descriptor2 = document.querySelector('.tagline-2')
 var randomButton = document.querySelector('.random-cover-button')
 var makeCoverButton = document.querySelector('.make-new-button')
-var saveCover = document.querySelector('.save-cover-button')
+var saveCoverButton = document.querySelector('.save-cover-button')
 var homeView = document.querySelector('.home-view')
 var formView = document.querySelector('.form-view')
 var homeButton = document.querySelector('.home-button')
-
+var viewCoversButton = document.querySelector('.view-saved-button')
+var savedView = document.querySelector('.saved-view')
 // We've provided a few variables below
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
@@ -21,15 +21,14 @@ var currentCover = new Cover(coverImgSrc, title, descriptor1, descriptor2)
 
 // Add your event listeners here👇
 window.addEventListener('load', generateBook)
-
 randomButton.addEventListener('click', generateBook)
-
 makeCoverButton.addEventListener('click', showForm)
-
+viewCoversButton.addEventListener('click', showSavedCovers)
+homeButton.addEventListener('click', showHome)
 
 // Create your event handlers and other functions here 👇
 function generateBook() {
-  newCover = new Cover(covers[getRandomIndex(covers)], titles[getRandomIndex(titles)], descriptor1[getRandomIndex(descriptors)], descriptor2[getRandomIndex(descriptors)])
+  newCover = new Cover(covers[getRandomIndex(covers)], titles[getRandomIndex(titles)], descriptors[getRandomIndex(descriptors)], descriptors[getRandomIndex(descriptors)])
   coverImgSrc.src = newCover.cover
   title.innerText = newCover.title
   descriptor1.innerText = newCover.tagline1
@@ -38,11 +37,26 @@ function generateBook() {
 function showForm() {
   homeView.classList.add('hidden')
   formView.classList.remove('hidden')
-  saveCover.classList.add('hidden')
+  saveCoverButton.classList.add('hidden')
   randomButton.classList.add('hidden')
   homeButton.classList.remove('hidden')
+}
+function showSavedCovers() {
+  homeView.classList.add('hidden')
+  savedView.classList.remove('hidden')
+  randomButton.classList.add('hidden')
+  saveCoverButton.classList.add('hidden')
+  homeButton.classList.remove('hidden')
+}
+function showHome() {
+  homeView.classList.remove('hidden')
+  savedView.classList.add('hidden')
+  formView.classList.add('hidden')
+  randomButton.classList.remove('hidden')
+  saveCoverButton.classList.remove('hidden')
 }
 // We've provided one function to get you started
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
 }
+console.log(getRandomIndex(descriptors))
